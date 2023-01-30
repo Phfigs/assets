888a64258c476834c44eadffd060a4dc7a1d2cf9
Pedro Figueiredo <104319499+Phfigs@users.noreply.github.com>
2023-01-30 16:36:15 -0300

    ex

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,3 @@
-const responsavel = document.querySelector("#responsavel");
-const cnpjEstabelecimento = document.querySelector("#cnpjEstabelecimento");
-const telefone = document.querySelector("#telefone");
-const email = document.querySelector("#email");
-const cidade = document.querySelector("#cidade");
-const tipoEstabelecimento = document.querySelector("#tipoEstabelecimento");
-const formulario = document.querySelector(".formulario");
-
-formulario.addEventListener("submit", function () {});
-
 let count = 1;
 
 document.getElementById("radio1").checked = true;
